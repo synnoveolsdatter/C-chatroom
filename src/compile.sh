892f2a503d03cb3914f6d0a978dd7f2ef0a33c48
Synnove <olsdattersynnove@gmail.com>
2023-11-16 13:07:20 -0500

    Fixed compile bug

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for sourcefile in `ls ./*.c`; do
-    echo gcc $sourcefile -o ${sourcefile/.c/} -Wall -Wpedantic
+    echo `gcc $sourcefile -o ${sourcefile/.c/} -Wall -Wpedantic`
 done
 echo ""
 echo "Finished"
